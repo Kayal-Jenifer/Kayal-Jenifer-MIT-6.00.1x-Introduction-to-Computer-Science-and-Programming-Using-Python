aded0d5cb70708c765eec09147bb6570e370a666
Kayal Jenifer <121043426+Kayal-Jenifer@users.noreply.github.com>
2023-04-06 21:51:23 +0530

    Add files via upload

---
+++ Problem set 6/Problem 4.py
@@ -0,0 +1,48 @@
+# Consider the following Python procedure. Specify its order of growth.
+
+def modten(n):
+    return n%10
+
+#answer: O(1)
+
+# Consider the following Python procedure. Specify its order of growth.
+
+def multlist(m, n):
+    '''
+    m is the multiplication factor
+    n is a list.
+    '''
+    result = []
+    for i in range(len(n)):
+        result.append(m*n[i])
+    return result
+
+#answer: O(len(n))
+
+# Consider the following Python procedure. Specify its order of growth.
+
+def foo(n):
+    if n <= 1:
+        return 1
+    return foo(n/2) + 1
+
+#answer: O(1)
+
+# Consider the following Python procedure. Specify its order of growth.
+
+def recur(n):
+    if n <= 0:
+        return 1
+    else:
+        return n*recur(n-1)
+
+#answer: O(n)
+
+# Consider the following Python procedure. Specify its order of growth.
+
+def baz(n):
+    for i in range(n):
+        for j in range(n):
+            print i,j
+
+#answer: O(n^2)
